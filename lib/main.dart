a69ac3e8b510d604ee2c49e8e5e122f07dd0d182
GioErco03 <75883612+GioErco03@users.noreply.github.com>
2023-04-26 14:38:45 +0200

    Fix image icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:MyMeteo/pages/homepage.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   runApp(const MainApp());
+  FlutterNativeSplash(
+    backgroundColor: Colors.white,
+    image: Image.asset('assets/custom_splash.png'),
+    imageBackgroundSize: BoxFit.contain,
+    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    loadingText: Text("Loading..."),
+    duration: 3000,
+    useLoader: true,
+    loaderColor: Colors.red,
+  );
 }
 
 class MainApp extends StatelessWidget {
